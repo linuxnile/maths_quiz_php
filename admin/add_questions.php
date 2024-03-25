4b3547b5822597d1155e9c1a31d4e4d3eb23fb0b
kaushik9288 <kaushik09288@gmail.com>
2024-03-25 23:05:45 +0530

    add question page changed

--- admin/add_questions.php
+++ admin/add_questions.php
@@ -4,6 +4,7 @@ session_start();
 $admin_id = $_SESSION['admin_id'];
 if(!isset($admin_id)){
    header('location:admin_login.php');
+   exit();
 }
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Retrieve form data
@@ -15,18 +16,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         'D' => $_POST['optionD']
     ];
     $answer = $_POST['answer'];
+    $category = $_POST['category'];
 
-    // Insert the new question
-    $insertResult = $questionsCollection->insertOne([
-        'question' => $questionText,
-        'options' => $options,
-        'answer' => $answer
-    ]);
-
-    if ($insertResult->getInsertedCount() == 1) {
-        echo "Question inserted successfully";
+    $existingQuestion = $questionsCollection->findOne(['question' => $questionText]);
+    if ($existingQuestion) {
+        $message[] = 'Question already exists!'; 
     } else {
-        echo "Error inserting question";
+        // Insert the new question
+        $insertResult = $questionsCollection->insertOne([
+            'question' => $questionText,
+            'options' => $options,
+            'answer' => $answer,
+            'category' => $category
+        ]);
+
+        if ($insertResult->getInsertedCount() == 1) {
+            $message[] = 'Question inserted successfully!';
+        } else {
+            $message[] = 'Failed to add question.';
+        }
     }
 }
 ?>
@@ -43,33 +51,44 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <body>
     <?php include 'admin_header.php' ?>
-    <h2>Insert a new MCQ Question</h2>
+    <section class="add-questions">
+    
     <form action="" method="post">
-        <label for="question">Question:</label><br>
-        <input type="text" id="question" name="question" required><br>
+    <h3>Insert a new MCQ Question</h3><br>
+        <!-- <label for="question">Question:</label><br> -->
+        <input type="text" id="question" name="question" placeholder="Enter question" class="box" required><br>
 
-        <label for="optionA">Option A:</label><br>
-        <input type="text" id="optionA" name="optionA" required><br>
+        <!-- <label for="optionA">Option A:</label><br> -->
+        <input type="text" id="optionA" name="optionA" placeholder="Option A" class="box" required><br>
 
-        <label for="optionB">Option B:</label><br>
-        <input type="text" id="optionB" name="optionB" required><br>
+        <!-- <label for="optionB">Option B:</label><br> -->
+        <input type="text" id="optionB" name="optionB" placeholder="Option B" class="box" required><br>
 
-        <label for="optionC">Option C:</label><br>
-        <input type="text" id="optionC" name="optionC" required><br>
+        <!-- <label for="optionC">Option C:</label><br> -->
+        <input type="text" id="optionC" name="optionC" placeholder="Option C" class="box" required><br>
 
-        <label for="optionD">Option D:</label><br>
-        <input type="text" id="optionD" name="optionD" required><br>
+        <!-- <label for="optionD">Option D:</label><br> -->
+        <input type="text" id="optionD" name="optionD" placeholder="Option D" class="box" required><br>
 
-        <label for="answer">Correct Answer:</label><br>
-        <select id="answer" name="answer" required>
+        <!-- <label for="answer">Correct Answer:</label><br> -->
+        <select id="answer" name="answer" class="box" required>
+            <option value="" disabled selected>--Select Correct Answer --</option>
             <option value="A">Option A</option>
             <option value="B">Option B</option>
             <option value="C">Option C</option>
             <option value="D">Option D</option>
-        </select><br><br>
+        </select><br>
+        <select id="category" name="category" class="box" required>
+            <option value="" disabled selected>--Select Category --</option>
+            <option value="easy">Easy</option>
+            <option value="normal">Normal</option>
+            <option value="hard">Hard</option>
+            <option value="expert">Expert</option>
+        </select><br>
 
-        <input type="submit" value="Insert Question">
+        <input type="submit" value="Insert Question" class="btn">
     </form>
+    </section>
     <script src="js/admin_script.js"></script>
 </body>
 
