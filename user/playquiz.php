ab07839b2a1869aabf917f271b43301ef925ef58
Linux Nile <linuxnile@gmail.com>
2024-03-26 04:00:39 -0700

    feat: add playquiz page

--- user/playquiz.php
+++ user/playquiz.php
@@ -1,70 +1,33 @@
-<?php
+<html>
 
-session_name("user");
-session_start();
+<head>
+    <title>Quiz Start</title>
+    <link rel="stylesheet" href="css/playquiz.css">
+</head>
 
-if (isset($_SESSION["signedin"]) == true) {
+<body>
+    <?php include('header.php'); ?>
 
-    require '../vendor/autoload.php';
+    <div class="quiz-container">
+        <h1>Welcome to the Quiz</h1>
+        <p>Test your knowledge by selecting a quiz level below:</p>
+        <div class="button-container">
+            <button class="btn level-easy">Easy</button>
+            <button class="btn level-medium">Medium</button>
+            <button class="btn level-hard">Hard</button>
+            <button class="btn level-expert">Expert</button>
+        </div>
+        <p>Choose your challenge wisely and enjoy!</p>
+        <div class="quiz-info">
+            <h2>How to Play:</h2>
+            <ul>
+                <li>Choose your desired quiz level by clicking on one of the buttons above.</li>
+                <li>Once you select a level, you'll be presented with a series of questions.</li>
+                <li>Answer each question to the best of your ability.</li>
+                <li>After answering all questions, submit your answers and see how well you did!</li>
+            </ul>
+        </div>
+    </div>
+</body>
 
-    $client = new MongoDB\Client("mongodb://localhost:27017");
-    $quizDB = $client->selectDatabase('mathsquiz');
-    $questionsCollection = $quizDB->selectCollection('questions');
-
-    $questions = $questionsCollection->find()->toArray();
-
-    if (!isset($_SESSION['current_question'])) {
-        $_SESSION['current_question'] = 0;
-        $_SESSION['score'] = 0;
-    }
-
-    if (isset($_POST['submit'])) {
-        $currentQuestion = $questions[$_SESSION['current_question']];
-        if ($_POST['answer'] === $currentQuestion['answer']) {
-            $_SESSION['score']++;
-        }
-        $_SESSION['current_question']++;
-    }
-
-    $currentQuestion = $questions[$_SESSION['current_question']] ?? null;
-
-    if ($_SESSION['current_question'] >= count($questions)) {
-        echo "Quiz completed! Your score: " . $_SESSION['score'];
-        session_destroy();
-        exit();
-    }
-?>
-
-    <html>
-
-    <head>
-        <title>Play the Quiz</title>
-    </head>
-
-    <body>
-        <?php include('header.php'); ?>
-
-        <?php if ($currentQuestion) : ?>
-            <form method="post">
-                <p><?php echo $currentQuestion['question']; ?></p>
-                <?php foreach ($currentQuestion['options'] as $key => $value) : ?>
-                    <label>
-                        <input type="radio" name="answer" required value="<?php echo $key; ?>">
-                        <?php echo $key . ': ' . $value; ?>
-                    </label><br>
-                <?php endforeach; ?>
-                <input type="submit" name="submit" value="Submit Answer">
-            </form>
-        <?php endif; ?>
-    </body>
-
-    </html>
-
-<?php } else {
-    echo
-    '<script>
-    alert("Please login to Start the Quiz!");
-    window.location.href = "login.php";
-</script>';
-}
-?>
+</html>
