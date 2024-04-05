d0fca12118aa92449bf037e3ecf12779e358ff33
Linux Nile <linuxnile@gmail.com>
2024-04-04 22:58:37 -0700

    fix: main content does not get hidden behind the fixed footer

--- user/footer.php
+++ user/footer.php
@@ -2,6 +2,7 @@
     <link rel="stylesheet" href="css/footer.css">
 </head>
 
+<div class="footer-spacer"></div>
 <footer>
     <div class="footer-content">
         <p>&copy; <?php echo date("Y"); ?> Maths Quiz for Kids</p>
